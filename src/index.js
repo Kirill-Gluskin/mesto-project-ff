f3e3ce781cec916aaea9258640284bc5f0db6373
Kirill Gluskin <kirill.gluskin@yandex.ru>
2024-11-30 13:23:57 +0400

    edit profile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 import './pages/index.css';
 import './scripts/card.js';
-import './scripts/modal.js';
+import {closeHundler, openPopHendler} from './scripts/modal.js';
+
+//элементы для вызова события при клике на кнопку редактирования
+const buttonEdit = document.querySelector('.profile__edit-button');
+const modalEdit = document.querySelector('.popup_type_edit');
+buttonEdit.addEventListener('click', () => openPopHendler(modalEdit))
+
+//элементы для вызова события при клике на кнопку плюс
+const buttonPlus = document.querySelector('.profile__add-button');
+const modalPlus = document.querySelector('.popup_type_new-card');
+buttonPlus.addEventListener('click', () => openPopHendler(modalPlus));
+
+//
+const formElement = document.forms['edit-profile']
+const nameInput = formElement.elements.name;
+const jobInput = formElement.elements.description;
+
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileDescription.textContent;
+
+    function handleFormSubmit(evt) {
+        evt.preventDefault();
+        profileTitle.textContent = nameInput.value;
+        profileDescription.textContent = jobInput.value;
+        closeHundler(modalEdit);
+    }
+
+formElement.addEventListener('submit', handleFormSubmit);
 
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,21 +1,25 @@
-const buttonEdit = document.querySelector('.profile__edit-button')
-const modalEdit = document.querySelector('.popup_type_edit')
-
-function openPopHendler (modal) {
+export function openPopHendler (modal) {
     modal.classList.add ('popup_is-opened');
     const buttonClose = modal.querySelector('.popup__close');
     buttonClose.addEventListener('click', () => closeHundler(modal));
     document.addEventListener('keydown', evt => escKeyHundler(modal, evt));
+    modal.addEventListener('mousedown', evt => modalOverlayHundler(modal, evt))
 }
 
-function closeHundler (modal) {
+export function closeHundler (modal) {
     modal.classList.remove('popup_is-opened');
 }
 
-function escKeyHundler (modal, evt) {
+export function escKeyHundler (modal, evt) {
     if (evt.keyCode === 27) {
         closeHundler(modal)
     }
 }
 
-buttonEdit.addEventListener('click', () => openPopHendler(modalEdit))
+export function modalOverlayHundler (modal, evt) {
+    const modalContent = modal.querySelector('.popup__content');
+    if (!modalContent.contains(evt.target)) {
+        closeHundler(modal)
+    }
+}
+
